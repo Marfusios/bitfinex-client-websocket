b441b47ccc7834578ea6385a3844a59d555250ef
Mariusz Kotas <m@mkotas.cz>
2018-01-22 12:45:06 +0100

    Cleanup after merge.

--- src/Bitfinex.Client.Websocket/Requests/CandlesSubscribeRequest.cs
+++ src/Bitfinex.Client.Websocket/Requests/CandlesSubscribeRequest.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using Bitfinex.Client.Websocket.Utils;
+﻿using Bitfinex.Client.Websocket.Utils;
 using Bitfinex.Client.Websocket.Validations;
 
 namespace Bitfinex.Client.Websocket.Requests
 {
     public class CandlesSubscribeRequest : SubscribeRequestBase
     {
-        public override string Channel => "candles";
-        public string Key { get; set; }
-
-
         public CandlesSubscribeRequest(string pair, BitfinexTimeFrame bitfinexTimeFrame)
         {
             BfxValidations.ValidateInput(pair, nameof(pair));
@@ -17,19 +12,12 @@ namespace Bitfinex.Client.Websocket.Requests
             Key = $"trade:{TimeFrameToKeyCommand(bitfinexTimeFrame)}:{FormatPairToSymbol(pair)}";
         }
 
+        public override string Channel => "candles";
+        public string Key { get; set; }
+
         private string TimeFrameToKeyCommand(BitfinexTimeFrame bitfinexTimeFrame)
         {
             return bitfinexTimeFrame.GetStringValue();
-        }
-
-        private string FormatPairToSymbol(string pair)
-        {
-            var formatted = pair
-                .Trim()
-                .Replace("/", string.Empty)
-                .Replace("\\", string.Empty)
-                .ToUpper();
-            return $"t{formatted}";
         }
     }
 }
