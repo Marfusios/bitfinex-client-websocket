206618c5410fc6c7d38d3e28aba28d66971781f3
Mariusz Kotas <m@mkotas.cz>
2019-01-26 00:37:51 +0100

    Fix configuration request

--- src/Bitfinex.Client.Websocket/Requests/ConfigurationRequest.cs
+++ src/Bitfinex.Client.Websocket/Requests/ConfigurationRequest.cs
@@ -1,4 +1,5 @@
 ﻿using Bitfinex.Client.Websocket.Messages;
+using Newtonsoft.Json;
 
 namespace Bitfinex.Client.Websocket.Requests
 {
@@ -7,6 +8,22 @@ namespace Bitfinex.Client.Websocket.Requests
     /// </summary>
     public class ConfigurationRequest : RequestBase
     {
+        /// <summary>
+        /// Request to configure websocket connection
+        /// </summary>
+        public ConfigurationRequest()
+        {
+        }
+
+        /// <summary>
+        /// Request to configure websocket connection
+        /// </summary>
+        /// <param name="flags">Flags - the bitwise XOR of the different options</param>
+        public ConfigurationRequest(ConfigurationFlag flags)
+        {
+            SelectedFlags = flags;
+        }
+
         /// <inheritdoc />
         public override MessageType EventType => MessageType.Conf;
 
@@ -16,6 +33,12 @@ namespace Bitfinex.Client.Websocket.Requests
         /// Example:
         /// To enable all decimals as strings and all times as date strings, you would set the value of flags to 40
         /// </summary>
-        public ConfigurationFlag Flags { get; set; }
+        [JsonIgnore]
+        public ConfigurationFlag SelectedFlags { get; set; }
+
+        /// <summary>
+        /// Flags - converted to integer
+        /// </summary>
+        public int Flags => (int)SelectedFlags;
     }
 }
