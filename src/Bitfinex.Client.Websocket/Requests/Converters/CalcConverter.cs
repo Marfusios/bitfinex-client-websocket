5c67c9126caee3bee9916296fd4fd1afd1af6ef6
Guido Klijn <guidoklijn@live.nl>
2019-03-29 12:08:41 +0100

    Add calc request

---
+++ src/Bitfinex.Client.Websocket/Requests/Converters/CalcConverter.cs
@@ -0,0 +1,44 @@
+using System;
+using Bitfinex.Client.Websocket.Exceptions;
+using Newtonsoft.Json;
+
+namespace Bitfinex.Client.Websocket.Requests.Converters
+{
+    public class CalcConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (!(value is CalcRequest calcRequest))
+                throw new BitfinexBadInputException("Can't serialize calcRequest");
+
+
+            writer.WriteStartArray();
+            writer.WriteValue(0);
+            writer.WriteValue("calc");
+            writer.WriteValue((object) null);
+
+            writer.WriteStartArray();
+
+            foreach (var request in calcRequest.Requests)
+            {
+                writer.WriteStartArray();
+                writer.WriteValue(request);
+                writer.WriteEndArray();
+            }
+
+            writer.WriteEndArray();
+            writer.WriteEndArray();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(CalcRequest);
+        }
+    }
+}
